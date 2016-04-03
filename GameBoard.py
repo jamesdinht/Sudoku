86e774523d99bb0c708f9c65cc9cb4c8594e7da9
James Dinh <jamesdinh14@gmail.com>
2016-04-03 14:01:34 -0700

    Begin implementation of Penciling In Algorithm

--- GameBoard.py
+++ GameBoard.py
@@ -17,28 +17,8 @@ class GameBoard:
 			print()
 		print("\n")
 
-	# Crosshatching algorithm to solve Sudoku Game
-	def crosshatch(self, value):
-		colIndeces = []
-		cellValue = str(value)
-		for row in self.grid:
-			for cell in row:
-				# print(cell.value, " ", value)
-				if cell.value == str(value):
-				# Mark rows
-					for cell in row:
-						cell.mark()
-					#Create list of columns to be marked
-					colIndeces.append(row.index(SudokuCell(str(value))))
-		# Mark each column in the grid according to the list we generated
-		for row in self.grid:
-			for index in colIndeces:
-				row[index].mark()
-
-		return self
-
 	# check if all cells are marked
-	def isAllMArked(self):
+	def isAllMarked(self):
 		for row in self.grid:
 			for cell in row:
 				if not cell.marked:
@@ -51,10 +31,51 @@ class GameBoard:
 			for cell in row:
 				cell.unmark()
 
+	# Crosshatching algorithm to solve Sudoku Game
+	def crosshatch(self, value, mark=True):
+		colIndeces = []
+		cellValue = str(value)
+		for row in self.grid:
+			for cell in row:
+				# print(cell.value, " ", value)
+				if cell.value == cellValue:
+					if mark:
+						# Mark rows
+						for cell in row:
+							cell.mark()
+						#Create list of columns to be marked
+						colIndeces.append(row.index(SudokuCell(str(value))))
+					else:
+						if cellValue in cell.pencilInList:
+							cell.pencilInList.remove(cellValue)
+		# Mark each column in the grid according to the list we generated
+		for row in self.grid:
+			for index in colIndeces:
+				if mark:
+					row[index].mark()
+				else:
+					row[index].pencilInList.remove(cellValue)
+
+		return self
+
+	def pencilIn(self, pencilInList, nonetRow, nonetCol):
+		for i in range(0, 2 + 1):
+			for j in range(0, 2 + 1):
+				if self.grid[nonetRow + i][nonetCol + j].isBlank():
+					self.grid[nonetRow + i][nonetCol + j].initPencilInList(pencilInList)
+					for num in pencilInList:
+						self.crosshatch(num, mark=False)
+		return
+
+	def fillIn(self, row, col, replacementValue):
+		self.grid[row][col].value = replacementValue
+
 	# Function makes possible changes
 	def solve(self):
 		queue = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-		while len(queue) > 0:
+		numOfPasses = 0
+
+		while queue and numOfPasses <= 18:
 			self = self.crosshatch(queue[0])
 			# look at nonet
 			# if there is ONE AND ONLY ONE zero, change that zero to queue[0]
@@ -63,14 +84,19 @@ class GameBoard:
 				for colIndex in range(0, 6 + 1, 3):
 					numBlankSpaces = 0
 					makeChanges = duplicateInNonet = False
+					pencilInList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 					for i in range(0, 2 + 1):
 						for j in range(0, 2 + 1):
 							# if you find a blank space, save its row and column and increase num of blank spaces
 							if (not self.grid[rowIndex + i][colIndex + j].marked) and self.grid[rowIndex + i][colIndex + j].value == "0":
-								print("Blank space found")
 								blankRow = rowIndex + i
 								blankCol = colIndex + j
 								numBlankSpaces += 1
+
+							# Prepare penciling in list for all blank spaces in a single nonet
+							if self.grid[rowIndex + i][colIndex + j].value in pencilInList:
+								pencilInList.remove(self.grid[rowIndex + i][colIndex + j].value)
 
 							# IF THE VALUE IS FOUND IN THE NONET YOU NEED TO BUST OUT OF THERE IMMEDIATELY
 							if self.grid[rowIndex + i][colIndex + j].value == queue[0]:
@@ -82,23 +108,18 @@ class GameBoard:
 							# otherwise, you can make a change
 							else:
 								makeChanges = True		
-					# print()
 
 					if makeChanges and not duplicateInNonet:
-						# print("Making change to grid")
-						self.grid[blankRow][blankCol].value = queue[0]
+						self.fillIn(blankRow, blankCol, queue[0])
 						self = self.crosshatch(queue[0])
-					# else:
-					# 	print("Not making changes")
 
+					self.pencilIn(pencilInList, rowIndex, colIndex)
 
 			# if every element is marked, remove that value from the Queue
-			if self.isAllMArked():
-				print("Popping Queue", queue[0])
+			if self.isAllMarked():
 				queue.pop(0)
 			# else, move that value to the back of the queue
 			else:
-				print("Moving ", queue[0], " to back of Queue")
 				queue.append(queue.pop(0))
 
 			self.unmarkAll()
@@ -109,13 +130,20 @@ class GameBoard:
 class SudokuCell:
 	def __init__(self,value):
 		self.value = value
+		self.pencilInList = []
 		self.marked = False
+
+	def initPencilInList(self, pencilInList):
+		self.pencilInList = pencilInList
 
 	def mark(self):
 		self.marked = True
 
 	def unmark(self):
 		self.marked = False
+
+	def isBlank(self):
+		return self.value == '0'
 
 	def __eq__(self, other):
 		return self.value == other.value
