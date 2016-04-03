e7a5d389fc63ccd714e4727741033acdb3ef5529
Brett Lockhart <blockhart615@gmail.com>
2016-04-02 21:51:49 -0700

    parse file into list of sudoku grids

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,26 @@
-#Python
+import sys
 
 def getSudokuGrid(fileName):
 	sudokuFile = open(fileName, 'r')
-	
-	for line in sudokuFile:
-		print(line)
+	gridList = []
+	sudokuGrid = ""
 
+	# reads in each line of the sudoku file
+	for line in sudokuFile:
+		# if the line is part of the sudoku grid, save it in sudokuGrid
+		if line.isdigit():
+			sudokuGrid += line
+		# ignores \n at the end of a line.
+		elif line[:-1].isdigit():
+			sudokuGrid += line[:-1]
+		# when a grid is complete, add it to the gridList
+		if len(sudokuGrid) == 81:
+			gridList.append(list(sudokuGrid))
+			sudokuGrid = ""
+
+# MAIN METHOD
 def main(fileName):
 	getSudokuGrid(fileName)
 
 if __name__ == '__main__':
-	main('sudoku.txt')
+	main(sys.argv[1])
