ea11a98a57c99041cea9abff73f2f08a03d9e3cf
James Dinh <jamesdinh14@gmail.com>
2016-04-02 20:16:57 -0700

    Begin work on file reading

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,13 @@
 #Python
+
+def getSudokuGrid(fileName):
+	sudokuFile = open(fileName, 'r')
+	
+	for line in sudokuFile:
+		print(line)
+
+def main(fileName):
+	getSudokuGrid(fileName)
+
+if __name__ == '__main__':
+	main('sudoku.txt')
